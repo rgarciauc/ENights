bb521675e438c965e084a444f74bec8230bcf04c
Rubén García <28358796+rgarciauc@users.noreply.github.com>
2024-02-02 22:41:45 +0100

    feature/applicationstartedup three hotelrooms are created

---
+++ hotelmanagerBE/src/main/java/com/garcia/hotelmanager/DataInitializer.java
@@ -0,0 +1,33 @@
+package com.garcia.hotelmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.garcia.hotelmanager.dao.HotelRepository;
+import com.garcia.hotelmanager.dao.HotelRoomRepository;
+import com.garcia.hotelmanager.model.RoomSize;
+import com.garcia.hotelmanager.service.HotelService;
+
+@Component
+public class DataInitializer implements CommandLineRunner{
+	
+	@Autowired
+	private HotelRoomRepository hotelRoomRepository;
+	@Autowired
+	private HotelRepository hotelRepository;
+	
+	@Autowired
+    private HotelService hotelService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		hotelRoomRepository.deleteAll();
+		hotelRepository.deleteAll();
+		
+		hotelService.createHotelRoom(RoomSize.DOUBLE, true);
+		hotelService.createHotelRoom(RoomSize.SINGLE, true);
+	    hotelService.createHotelRoom(RoomSize.SUITE, false);
+	}
+
+}
