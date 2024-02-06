35ccb4021147be566b233038bb98392884bf0461
Rubén García <28358796+rgarciauc@users.noreply.github.com>
2024-02-06 01:23:15 +0100

    feature/addsearch by id

--- hotelmanagerFE/pages/index.vue
+++ hotelmanagerFE/pages/index.vue
@@ -5,10 +5,32 @@
         </div>
         <div v-else class="container mx-auto bg-darkBackground text-customGreen">
             <h1 class="text-center text-5xl text-white my-6">Hotel Room List</h1>
-            <label>
-                Show Only Free Rooms
-                <input type="checkbox" v-model="showOnlyFreeRooms" />
-            </label>
+            <div class="flex flex-row my-4">
+                <div class="filter flex-1">
+                    <label>
+                        Show Only Free Rooms
+                        <input type="checkbox" v-model="showOnlyFreeRooms" />
+                    </label>
+                </div>
+
+                <div class="search flex-2">
+                    <input v-model="searchTerm" placeholder="Search by Room ID" @input="handleInput" />
+                    <button @click="searchRoom">Search</button>
+                    <div v-if="selectedRoom">
+                        <div class="card">
+                            <button class="close-button" @click="closeCard">x</button>
+                            <p>Room Size: {{ selectedRoom.roomSize }}</p>
+                            <p>Minibar: {{ selectedRoom.hasMinibar ? 'Yes' : 'No' }}</p>
+                            <p>Availability: {{ selectedRoom.isFree ? 'Free' : 'Occupied' }}</p>
+                            <p>Number: {{ selectedRoom.id }}</p>
+                        </div>
+                    </div>
+                    <div v-if="showNotFound" class="not-found-message">
+                        {{ notFoundMessage }}
+                    </div>
+                </div>
+            </div>
+
             <div class="flex flex-col items-center bg-slate-700">
                 <div class="my-6">
                     <HotelRoomCard class="min-w-96" v-for="hotelRoom in filteredRooms" :key="hotelRoom.id"
@@ -52,6 +74,10 @@
             editMode: false,
             showOnlyFreeRooms: false,
             editedRoom: {},
+            searchTerm: '',
+            selectedRoom: null,
+            notFoundMessage: '',
+            showNotFound: false,
         };
     },
     computed: {
@@ -99,7 +125,67 @@
             }
         
         },
+        async searchRoom(){
+            try {
+                const roomNumber = parseInt(this.searchTerm);
+                if (isNaN(roomNumber)) {
+                    this.showNotFoundMessage("Please enter a valid room number.");
+                    return;
+                }
+                const response = await this.$axios.get(`/api/hotel/rooms/${this.searchTerm}`);
+                if(response.status === 200) {
+                    this.selectedRoom = response.data;
+                    console.log(response.data)
+                }
+            } catch (error) {
+                if (error.response && error.response.status === 404) {
+                    const errorMessage = error.response.data;
+                    this.showNotFoundMessage(errorMessage || "not found!");
+                } else if (error.request){
+                    this.showNotFoundMessage("The server is currently down.");
+                }
+                else {
+                    console.error('Error fetching hotel room:', error);
+                }
+            }
+        },
+        showNotFoundMessage(message){
+            this.showNotFound = true;
+            console.log(this.showNotFound);
+            this.notFoundMessage = message;
+            console.log(this.notFoundMessage)
+            setTimeout(()=> {
+                this.notFoundMessage = '';
+                this.showNotFound = false;
+            }, 2000);
+        },
+        closeCard() {
+            this.selectedRoom = null;
+            this.searchTerm = '';
+        },
+        handleInput() {
+            if (!this.searchTerm) {
+                this.closeCard();
+                this.notFoundMessage = '';
+            }
+        },
     },
   };
   </script>
+<style scoped>
+.card {
+    border: 1px solid #ccc;
+    padding: 20px;
+    margin: 10px;
+    border-radius: 8px;
+    position: relative;
+}
+
+.close-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+}
+</style>
   
