28b4aa4f1650856454b01efb07ae1a54ee6634cc
Rubén García <28358796+rgarciauc@users.noreply.github.com>
2024-02-09 05:24:52 +0100

    Fix Dockerization

--- hotelmanagerFE/nuxt.config.js
+++ hotelmanagerFE/nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.SPRING_BOOT_SERVER_URL,
     headers: {
       common: {
         'Content-Type': 'application/json',
